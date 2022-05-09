f03e56eb83fb8f5375c7d5cff92135d22b6da4a7
BindingOx <mbonthera@gmail.com>
2022-05-09 17:26:28 +0700

    Create todo list of steps to accomplish

--- chapter2/practice/mybattleship.js
+++ chapter2/practice/mybattleship.js
@@ -26,3 +26,16 @@ let isSunk = false; // keep track of whether the ship is sunk or not. set it to 
 // 	} //end else
 // } //end loop
 // tellUserStats;
+
+// TODO: Step 1 - Create loop and get user guess
+// - The loop. It needs to keep looping while the ship isn’t sunk. 
+
+// TODO: Step 2 - Check user guess
+// - Get the guess from the user and validate it.
+// - Make sure it really is a number between 0 and 6
+
+// TODO: Step 3 - Check if ship has sunk
+// - Write the logic to check for a hit on a ship and see if the ship is sunk.
+
+// TODO: Step 4 - Display states to user
+// - Create a little report for the user with the number of guesses it took to sink the ship.
