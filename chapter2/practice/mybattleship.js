ddc5afa49bbc81a109ca3b6ca73509d1992f4db4
BindingOx <mbonthera@gmail.com>
2022-05-09 14:02:33 +0700

    Write pseudocode

---
+++ chapter2/practice/mybattleship.js
@@ -0,0 +1,18 @@
+// Pseudocode
+// while (ship isn't sunk){
+// 	getusersguess;
+// 	compareusersinput;
+// 	if (valid input?) {
+// 	    ADD one to guesses;
+// 		if (user’s guess matches a location) {
+// 			ADD one to the number of hits;
+// 			if (number of hits is 3) {
+// 				SET isSunk to true;
+// 				TELL user "You sank my battleship!";
+// 			} //end if
+// 		} //end if
+// 	} else {
+// 	    "Please enter a valid number";
+// 	} //end else
+// } //end loop
+// tellUserStats;
